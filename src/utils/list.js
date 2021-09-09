875b78dbb63513c56efcfc3cc8e0bf22a16d8ccf
Alef <oa.g11@icloud.com>
2021-09-09 06:12:01 -0500

    Add section header

--- src/utils/list.js
+++ src/utils/list.js
@@ -1,5 +1,12 @@
 const container = document.getElementById('scoreList');
 
+const header = `
+<div class="header">
+  <h2>Recent Scores</h2>
+  <button id="refresh" >Refresh</button>
+</div>
+`
+
 const listMarkup = `
 <div class="score-list">
   <ul class="scores">
@@ -12,7 +19,8 @@ const listMarkup = `
 `;
 
 const renderScoreList = () => {
-  container.insertAdjacentHTML('afterbegin', listMarkup);
+  container.insertAdjacentHTML('afterbegin', header);
+  container.insertAdjacentHTML('beforeend', listMarkup);
 };
 
 export default renderScoreList;
