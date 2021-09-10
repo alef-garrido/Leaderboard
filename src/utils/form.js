20cfae88aea339eff267d92c0e07311a5a14fd4b
Alef <oa.g11@icloud.com>
2021-09-10 09:01:00 -0500

    fix validation

--- src/utils/form.js
+++ src/utils/form.js
@@ -3,9 +3,9 @@ const container = document.getElementById('addScore');
 const addForm = `
 <h2>Add your Score</h2>
 <form id="add-Score">
-  <input type="text" name="user"  class="form-control" placeholder="Your name">
-  <input type="text" name="score" class="form-control" placeholder="Your score">     
-  <input  class="addScore btn btn-primary" type="button" value="Submit!">
+  <input type="text" required="required" name="user"  class="form-control" placeholder="Your name">
+  <input type="text" required="required" name="score" class="form-control" placeholder="Your score">     
+  <input  class="addScore btn btn-primary" type="submit" value="Submit!">
 </form>
 `;
 const renderForm = () => {
