7e7c36d4249c920595d2188566717cbfd417ba21
Alef <oa.g11@icloud.com>
2021-09-10 06:46:32 -0500

    Make inputs required using HTML5  validation

--- src/utils/form.js
+++ src/utils/form.js
@@ -3,8 +3,8 @@ const container = document.getElementById('addScore');
 const addForm = `
 <h2>Add your Score</h2>
   <form id="addScore">
-    <input type="text" name="user" placeholder="Your name">
-    <input type="text" name="score" placeholder="Your score">
+    <input type="text" name="user" placeholder="Your name" required>
+    <input type="text" name="score" placeholder="Your score" required>
     <input  class="addScore" type="button" value="submit">
    </form>
 `;
